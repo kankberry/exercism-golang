8d85da983036f34af06bf38d51f3ee60e55ba31b
Craig Johnson <craigdevjohnson@gmail.com>
2020-07-06 19:28:45 -0800

    Adding reverse_string exercism

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,17 @@
+package reverse
+
+import (
+	"unicode/utf8"
+)
+
+//Reverse takes the input of a string and returns the reverse of it
+func Reverse(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
+}
